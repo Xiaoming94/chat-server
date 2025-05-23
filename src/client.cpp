1e2e9c68983fee27f84181a7b43cdf6b391be08e
CsabaBeres <csaba.beres@nexergroup.com>
2025-05-23 14:58:16 +0200

    start a client

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 #include <string>
+#include <string_view>
+
+#include <sys/socket.h>
+
+constexpr int DOMAIN{AF_LOCAL};
+constexpr int TYPE{42};
+constexpr int PROTOCOL{0};
+
+class ClientImpl
+{
+public:
+    ClientImpl(std::string_view name) : 
+        m_socket(socket(DOMAIN, TYPE, PROTOCOL))
+    {
+        connect(name);
+    }
+
+    void connect(std::string_view name)
+    {
+        if (establishConnection())
+        {
+            onSuccessfulConnection();
+        }
+        else
+        {
+            onConnectionFailed();
+        }
+    }
+
+    bool establishConnection()
+    {
+        const sockaddr SERVER_NAME{.sa_family = AF_LOCAL, .sa_data = "127.0.0.1"};
+        const socklen_t length{};
+        if (::connect(m_socket, &SERVER_NAME, length) < 0)
+        {
+            onConnectionFailed();
+            return false;
+        }
+        return false;
+    }
+
+    void onConnectionFailed()
+    {
+        std::cerr << "Connection establishment failed\n";
+        std::abort();
+    }
+
+    void onSuccessfulConnection()
+    {
+        //listen to the UI
+        //listen to the server
+    }
+
+private:
+    int m_socket;
+};
 
 int main(int numberOfArguments, char ** args) {
     if (numberOfArguments != 2u)
@@ -8,67 +64,7 @@ int main(int numberOfArguments, char ** args) {
         std::abort();
     }
     std::string name{args[1]};
-    std::cout << "Client startin'..." << name << std::endl;
+    std::cout << "Client starting..." << name << std::endl;
+    ClientImpl client(name);
     return 0;
 }
-
-//two users chat each other
-//Given: one client sends a message to another client
-//And:  if the other client is off line
-//When: sender has no confirmation (yet)
-//Then: ....
-
-//there is a pending message
-//the client starts
-//the message is displayed (optionally with the sender)
-
-//the client is running
-//the client receives a message
-//the message is displayed (optionally with the sender)
-
-// client:
-// identify itself
-// start up (subscription, connect to the server) > receive messages 
-// send
-
-// server
-// start up 
-// receive and forward or store messages 
-
-// ./client userId
-// Client starting...
-// send ravi Hello 
-// To ravi: Hello 
-// From ravi: Hej to you as well
-
-class ClientUI
-{
-    public:
-    //display output 
-    virtual void print(std::string_view message) const = 0;
-};
-
-class ClientUIStdOutput : public ClientUI
-{
-public:
-    void print(std::string_view message) const override
-    {
-        std::cout << message << '\n';        
-    }
-};
-
-class ClientBackEnd
-{
-public:
-    //connect to server
-    virtual void start(std::string_view name) = 0;
-
-    virtual void send(std::string_view message, std::string_view to) const = 0;
-};
-
-class ClientBackEndImpl : public ClientBackEnd
-{
-
-    private:
-    std::reference_wrapper<ClientUI> m_ui;
-};
--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <sys/socket.h>
+
+constexpr int DOMAIN{AF_LOCAL};
+constexpr int TYPE{42};
+constexpr int PROTOCOL{0};
+
+class Server
+{
+public:
+    Server() :
+        m_socket(socket(DOMAIN, TYPE, PROTOCOL))
+    {}
+
+    void start()
+    {
+        //publish itself
+        const sockaddr SERVER_NAME{.sa_family = AF_LOCAL, .sa_data = "127.0.0.1"};
+        const socklen_t length{};
+
+        if (bind(m_socket, &SERVER_NAME, length) < 0)
+        {
+            std::cerr << "The server cannot be started\n";
+            std::abort();
+        }
+    }
+
+private:
+    int m_socket;
+};
 
 int main() {
     std::cout << "Server starting..." << std::endl;
+    Server server;
+    server.start();
     return 0;
 }
